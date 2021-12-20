c769ae01eefa7fc85f177edad07168860322b28a
mathlete01 <msallin@gmail.com>
2021-12-20 14:45:58 -0800

    Comment unused code

--- index.js
+++ index.js
@@ -10,29 +10,11 @@ const db = mysql.createConnection({
 });
 
 app.get("/", (req, res) => {
-	// var con = mysql.createConnection({
-	// 	host: "localhost",
-	// 	user: "root",
-	// 	password: "password",
-	// 	database: "formoctagon",
+	// const sqlInsert =
+	// 	"INSERT INTO users (firstName, lastName, email, zipcode, state) VALUES ('Jeremy', 'Sallin', 'jeremy@gmail.com', '90210', 'CA');";
+	// db.query(sqlInsert, (err, result) => {
+	// 	res.send("hello pedro");
 	// });
-
-	// con.connect(function (err) {
-	// 	if (err) throw err;
-	// 	console.log("Connected!");
-	// 	const sqlInsert =
-	// 		"INSERT INTO users (firstName, lastName, email, zipcode, state) VALUES ('Jeremy', 'Sallin', 'jeremy@gmail.com', '90210', 'CA');";
-	// 	con.query(sqlInsert, function (err, result) {
-	// 		if (err) throw err;
-	// 		console.log("1 record inserted");
-	// 	});
-	// });
-
-	const sqlInsert =
-		"INSERT INTO users (firstName, lastName, email, zipcode, state) VALUES ('Jeremy', 'Sallin', 'jeremy@gmail.com', '90210', 'CA');";
-	db.query(sqlInsert, (err, result) => {
-		res.send("hello pedro");
-	});
 });
 
 app.listen(3001, () => {
